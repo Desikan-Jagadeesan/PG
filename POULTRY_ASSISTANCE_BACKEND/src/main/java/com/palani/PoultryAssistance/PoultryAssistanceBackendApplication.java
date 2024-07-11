691292920cba51ac532f2ca22d43ada4f09ab03a
Desikan <Desikan@WINDOWS-IULBMN5>
2024-07-11 19:12:20 +0530

    COMMIT FOR PA BACKEND Project.

---
+++ POULTRY_ASSISTANCE_BACKEND/src/main/java/com/palani/PoultryAssistance/PoultryAssistanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.palani.PoultryAssistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoultryAssistanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoultryAssistanceBackendApplication.class, args);
+	}
+
+}
